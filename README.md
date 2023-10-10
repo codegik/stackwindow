f3df775cfe8af000a5e5d66b3a4b948179fdad22
Inacio Klassmann <iklassman@lendingclub.com>
2023-10-09 23:16:24 -0300

    adding stackwindow script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Stackwindow
+
+## Installing
+
+```shell
+mkdir -p ~/.hammerspoon/stackwindow
+git clone https://github.com/codegik/stackwindow.git ~/.hammerspoon/stackwindow
+echo 'require("stackwindow")' > ~/.hammerspoon/init.lua
+```
+
+## TODO
+
+- subscribe to events in order to recreate the stack
+  - creating window
+  - removing window
--- init.lua
+++ init.lua
@@ -1,55 +1,45 @@
-local stackMap = {}
-
+local stackedWindows = {}
 
 function iconFromAppName(app)
 	local appBundle = hs.appfinder.appFromName(app):bundleID()
 	return hs.image.imageFromAppBundle(appBundle)
 end
 
-
-function createStack(space)
-	if stackMap[space] then
-		for i, c in pairs(stackMap[space]) do
-			c:delete()
-		end
+function recreateStackWindow(space)
+	print("cleanning stacked windows")
+	for i, c in pairs(stackedWindows) do
+		hs.inspect(c)
+		c:delete()
 	end
 
-	local canvas = {}
+	stackedWindows = {}
+
 	local size = #hs.window.visibleWindows()
 	local startAt = 300
 
 	if size > 1 then
 		for i, window in pairs(hs.window.visibleWindows()) do
-			print("adding icon for " .. window:application():title())
-			canvas[i] = hs.canvas.new({ x = 0, y = startAt + 40 * i, h = 50, w = 50 })
-			canvas[i]:insertElement({
+			print("adding stacked window " .. window:application():title())
+			local canvas = hs.canvas.new({ x = 0, y = startAt + 40 * i, h = 50, w = 50 })
+			canvas:insertElement({
 				type = "image",
 				image = iconFromAppName(window:application():title()),
-				frame = {x = 0, y = 10, h = 30, w = 30},
-				imageAlpha = 0.5,
-				fillColor = { red = 1.0 }
+				frame = { x = 0, y = 10, h = 30, w = 30 },
+				imageAlpha = 1,
+				fillColor = { red = 1.0 },
 			})
-			canvas[i]:show()
+			canvas:show()
+			table.insert(stackedWindows, canvas)
 		end
-
-		canvas[1][1].imageAlpha = 1
-		stackMap[space] = canvas
-		print("added in space " .. space)
-	else
-		print("space is empty " .. space)
 	end
 end
-
 
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "r", function()
 	hs.reload()
 end)
 
-
-hs.spaces.watcher.new(function(s)
-	createStack(hs.spaces.focusedSpace())
-end):start()
-
-
-
-
+hs.spaces.watcher
+	.new(function(s)
+		recreateStackWindow(hs.spaces.focusedSpace())
+	end)
+	:start()
