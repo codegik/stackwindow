beee13fea3a49666be4df24b563aa379e9ffe339
Inacio Klassmann <iklassman@lendingclub.com>
2023-10-10 00:05:23 -0300

    adding stackwindow script

--- README.md
+++ README.md
@@ -5,11 +5,12 @@
 ```shell
 mkdir -p ~/.hammerspoon/stackwindow
 git clone https://github.com/codegik/stackwindow.git ~/.hammerspoon/stackwindow
-echo 'require("stackwindow")' > ~/.hammerspoon/init.lua
+echo 'require("stackwindow")' >> ~/.hammerspoon/init.lua
 ```
 
 ## TODO
 
+- subscribe to event in order to change focused window, then should change focused icon
 - subscribe to events in order to recreate the stack
   - creating window
   - removing window
--- init.lua
+++ init.lua
@@ -19,16 +19,21 @@ function recreateStackWindow(space)
 
 	if size > 1 then
 		for i, window in pairs(hs.window.visibleWindows()) do
-			print("adding stacked window " .. window:application():title())
+			print("adding: window = " .. window:title() .. ", id = " .. window:id())
 			local canvas = hs.canvas.new({ x = 0, y = startAt + 40 * i, h = 50, w = 50 })
-			canvas:insertElement({
-				type = "image",
-				image = iconFromAppName(window:application():title()),
-				frame = { x = 0, y = 10, h = 30, w = 30 },
-				imageAlpha = 1,
-				fillColor = { red = 1.0 },
-			})
-			canvas:show()
+			canvas
+				:insertElement({
+					type = "image",
+					image = iconFromAppName(window:application():title()),
+					frame = { x = 0, y = 10, h = 30, w = 30 },
+					imageAlpha = 0.5,
+					fillColor = { red = 1.0 },
+				})
+				:show()
+			if window:id() == hs.window.focusedWindow():id() then
+				print("hightlighing: window = " .. window:title() .. ", id = " .. window:id())
+				canvas[1].imageAlpha = 1
+			end
 			table.insert(stackedWindows, canvas)
 		end
 	end
